24952987f359ad955e1dbe3154064595d6cd4266
sunkaratarun9 <sunkaratarun9@gmail.com>
2025-08-04 21:04:58 +0530

    commit

--- src/BST/BST.java
+++ src/BST/BST.java
@@ -1,8 +1,10 @@
 package BST;
 
+import org.w3c.dom.Node;
+
 public class BST {
-    public class node{
-        private int val;
+    public class Node{
+        private final int val;
         private int height;
         private Node left;
         private Node right;
@@ -17,6 +19,32 @@ public class BST {
     public BST(){
 
     }
+    public int height(Node node){
+        if(node==null)
+            return -1;
+        return node.height;
+    }
+
+    public boolean isEmpty(){
+        return root==null;
+    }
+
+    public void display(){
+        display("Root Node: ",root);
+    }
+    public void display(String details,Node node ){
+        if(node==null) {
+            System.out.println(details + "null");
+            return;
+        }
+        else{
+            System.out.println(details + node.getVal());
+            display("Left Child Of " + node.getVal() + " : ", node.left);
+            display("Right Child Of " + node.getVal() + " : ", node.right);
+        }
+    }
+
+
 
 
 }
