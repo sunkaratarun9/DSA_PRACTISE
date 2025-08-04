5115680b9e4c049bba9c3e4a1589afad601fcd42
sunkaratarun9 <sunkaratarun9@gmail.com>
2025-08-04 22:02:24 +0530

    commit

--- src/BST/Main.java
+++ src/BST/Main.java
@@ -3,19 +3,20 @@ import BST.Bstpractise.*;
 public class Main  {
     public static void main(String[] args) {
         Bstpractise tree=new Bstpractise();
-//        System.out.println("isEmpty: "+tree.isEmpty());
+        System.out.println("isEmpty: "+tree.isEmpty());
 //        tree.insert(15);
 //        tree.insert(10);
 //        tree.insert(5);
 //        tree.insert(8);
 //        tree.insert(12);
 //        tree.insert(20);
-//        System.out.println("isEmpty: "+tree.isEmpty());
-//        System.out.println("isBalanced: "+tree.isBalanced());
-//        tree.insert(17);
-//        System.out.println("isBalanced: "+tree.isBalanced());
-        int[] arr={15,10,5,8,12,20,17};
+        int[] arr={15,10,5,8,12,20};
         tree.populate(arr);
+        System.out.println("isEmpty: "+tree.isEmpty());
+        System.out.println("isBalanced: "+tree.isBalanced());
+        tree.insert(17);
+        System.out.println("isBalanced: "+tree.isBalanced());
+
         tree.display();
 
 
